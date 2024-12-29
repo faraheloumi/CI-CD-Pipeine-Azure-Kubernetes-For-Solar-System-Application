b112a15c5ab7d0c0f1713b7a72869ed3618427cc
farah elloumi <farah.elloumi2000@gmail.com>
2024-12-30 00:24:32 +0100

    Update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ async function connectToDatabase() {
         await mongoose.connect(process.env.MONGO_URI, {
             user: process.env.MONGO_USERNAME,
             pass: process.env.MONGO_PASSWORD,
+            useUnifiedTopology: true,
         });
         console.log("MongoDB Connection Successful");
     } catch (err) {
